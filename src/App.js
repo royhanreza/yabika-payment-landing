3800b6ccc179524572e1227479af5939eb667882
royhanreza <royhanreza842@gmail.com>
2020-08-19 22:07:05 +0700

    remove nav

--- src/App.js
+++ src/App.js
@@ -13,22 +13,6 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Finish</Link>
-            </li>
-            <li>
-              <Link to="/unfinish">Unfinish</Link>
-            </li>
-            <li>
-              <Link to="/error">Error</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/" exact>
             <FinishPage />
